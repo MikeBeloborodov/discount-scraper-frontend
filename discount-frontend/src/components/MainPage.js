7701239141b9f42d96b4b20a1cf75c5bccc8e3c9
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2022-06-04 00:26:42 +0400

    added pagination

--- discount-frontend/src/components/MainPage.js
+++ discount-frontend/src/components/MainPage.js
@@ -10,20 +10,56 @@ export default function MainPage(){
         "column_4" : []
     })
 
+    const [pagination, setPagination] = React.useState([])
+
+    const [maxPage, setMaxPage] = React.useState(0)
+
+    const [pageSkip, setPageSkip] = React.useState(0)
+
+    const [currentPage, setCurrentPage] = React.useState(1)
+
     const [websiteButtons, setWebsiteButtons] = React.useState([])
 
     const [websiteFilter, setWebsiteFilter] = React.useState('')
 
     const URL = process.env.REACT_APP_API_URL
 
-    let num_of_columns = 4
-    let length_of_columns = 2
+    const num_of_columns = 4
+    const length_of_columns = 2
     
     function handle_website_filter(title, event){
+        setPageSkip(0)
+        setCurrentPage(1)
         setWebsiteFilter(title)
     }
 
+    function handle_pagination_filter(page_num, event){
+        setCurrentPage(page_num)
+        if (page_num === 1){
+            setPageSkip(0)
+            return
+        }
+        setPageSkip(oldValues => (page_num - 1) * (num_of_columns * length_of_columns))
+    }
+
+    function handle_next_page(e){
+        if (currentPage === maxPage){
+            return
+        }
+        setCurrentPage(oldValues => oldValues + 1)
+        setPageSkip(pageSkip + (num_of_columns * length_of_columns))
+    }
+
+    function handle_previous_page(e){
+        if (currentPage === 1){
+            return
+        }
+        setCurrentPage(oldValues => oldValues - 1)
+        setPageSkip(pageSkip - (num_of_columns * length_of_columns))
+    }
+
     React.useEffect(() => {
+        window.scrollTo(0, 0)
         // website buttons
         let website_button_elements
         fetch(URL + 'website')
@@ -43,10 +79,33 @@ export default function MainPage(){
                 setWebsiteButtons(website_button_elements)
             })
 
+        // pagination
+        let pagination_elements = []
+        fetch(URL + `promo/count?search=${websiteFilter}`)
+            .then(res => res.json())
+            .then(data => {
+                const num_of_pages = Math.ceil(data / (num_of_columns * length_of_columns))
+                setMaxPage(num_of_pages)
+                for (let i = 1; i <= num_of_pages; i++){
+                    pagination_elements.push(
+                        <li key={"li" + i}>
+                            <a 
+                            className={currentPage === i ? "pagination-link m-2 is-current danger" : "pagination-link m-2"}
+                            aria-label={`Goto page ${i}`}
+                            onClick={(e) => handle_pagination_filter(i, e)}
+                            >
+                                {i}
+                            </a>
+                        </li>
+                    )
+                }
+            })
+            setPagination(pagination_elements)
+
         // cards
         for (let i = 0; i < num_of_columns; i++){
             let elements
-            fetch(URL + `promo/slice?limit=${length_of_columns}&skip=${i * length_of_columns}&website=${websiteFilter}`, {method: "GET"})
+            fetch(URL + `promo/slice?limit=${length_of_columns}&skip=${(i * length_of_columns) + pageSkip}&website=${websiteFilter}`, {method: "GET"})
                 .then(res => res.json())
                 .then(data => {
                     elements = data.map(data => {
@@ -54,11 +113,11 @@ export default function MainPage(){
                             <div className='card' key={data.item_id}>
                                 <div className="card-image">
                                     <figure className="image is-4by3">
-                                        <img src={data.img} alt="Food item image"/>
+                                        <img src={data.img} alt="Food item image" className='cover-img'/>
                                     </figure>
                                 </div>
                                 <div className='content has-text-centered'>
-                                    <p className="title is-4 has-text-dark">{data.title}</p>
+                                    <p className="title is-4 has-text-dark mt-2">{data.title}</p>
                                     <p className="content has-text-dark">Вес: {data.weight ? data.weight : "Не указан"}</p>
                                     {data.old_price ? 
                                         <div className='columns is-mobile'>
@@ -86,18 +145,25 @@ export default function MainPage(){
                     })
                 })
             }
-        }, [websiteFilter])
+        }, [websiteFilter, pageSkip])
 
     return(
         <>
             <Navbar />
+
             <section className='section'>
                 <div className='box'>
-                    <p className='content'>Сортировать по сайтам:</p>
-                    {websiteButtons}
+                        <p className='content'>Сортировать по сайтам:</p>
+                        <button 
+                            className='button is-danger is-outlined is-rounded m-2' 
+                            onClick={(e) => handle_website_filter('', e)}>
+                                Все сайты
+                            </button>
+                        {websiteButtons}
                 </div>
             </section>
-            <section className='section'>
+
+            <section className='section mt-0 mb-0 pt-0 pb-0'>
                 <div className='columns'>
                     <div className='column'>
                         {columns.column_1}
@@ -113,6 +179,31 @@ export default function MainPage(){
                     </div>
                 </div>
             </section>
+
+            <section className='section mt-0 pt-0'>
+                <div className='box'>
+                        <div className='block'>
+                            <a 
+                            className="pagination-previous"
+                            onClick={(e) => {handle_previous_page(e)}}
+                            >
+                                Previous
+                            </a>
+                            <a 
+                            className="pagination-next"
+                            onClick={(e) => {handle_next_page(e)}}
+                            >
+                            Next page
+                            </a>
+                        </div>
+                    <nav className="pagination is-rounded" role="navigation" aria-label="pagination">
+                    </nav>
+                        <ul className="pagination-list">
+                            {pagination}
+                        </ul>
+                </div>
+            </section>
+
             <Footer />
         </>
     )
